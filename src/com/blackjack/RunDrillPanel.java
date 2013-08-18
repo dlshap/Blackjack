c87e1137398e86fc413e06f51a537625329e9178
David Shapiro <DavidS@dlsa.com>
2013-08-18 15:14:09 -0500

    a little work on the panel

--- src/com/blackjack/RunDrillPanel.java
+++ src/com/blackjack/RunDrillPanel.java
@@ -56,7 +56,7 @@ public class RunDrillPanel {
 			c.fill = GridBagConstraints.HORIZONTAL;
 		}
 
-		button = new JButton("Button 1");
+		button = new JButton("Hit");
 		if (shouldWeightX) {
 			c.weightx = 0.5;
 		}
@@ -65,36 +65,43 @@ public class RunDrillPanel {
 		c.gridy = 0;
 		pane.add(button, c);
 
-		button = new JButton("Button 2");
+		button = new JButton("Stand");
 		c.fill = GridBagConstraints.HORIZONTAL;
 		c.weightx = 0.5;
 		c.gridx = 1;
 		c.gridy = 0;
 		pane.add(button, c);
 
-		button = new JButton("Button 3");
+		button = new JButton("Split");
 		c.fill = GridBagConstraints.HORIZONTAL;
 		c.weightx = 0.5;
 		c.gridx = 2;
 		c.gridy = 0;
 		pane.add(button, c);
 
-		button = new JButton("Long-Named Button 4");
+		button = new JButton("Double");
 		c.fill = GridBagConstraints.HORIZONTAL;
-		c.ipady = 40; // make this component tall
-		c.weightx = 0.0;
-		c.gridwidth = 3;
-		c.gridx = 0;
-		c.gridy = 1;
+		c.weightx = 0.5;
+		c.gridx = 3;
+		c.gridy = 0;
 		pane.add(button, c);
+		
+//		button = new JButton("Deal");
+//		c.fill = GridBagConstraints.HORIZONTAL;
+//		c.ipady = 40; // make this component tall
+//		c.weightx = 0.5;
+//		c.gridwidth = 4;
+//		c.gridx = 0;
+//		c.gridy = 1;
+//		pane.add(button, c);
 
-		button = new JButton("5");
+		button = new JButton("Deal");
 		c.fill = GridBagConstraints.HORIZONTAL;
 		c.ipady = 0; // reset to default
 		c.weighty = 1.0; // request any extra vertical space
 		c.anchor = GridBagConstraints.PAGE_END; // bottom of space
-		c.insets = new Insets(10, 0, 0, 0); // top padding
-		c.gridx = 1; // aligned with button 2
+		c.insets = new Insets(150, 0, 0, 0); // top padding
+		c.gridx = 0; // aligned with button 2
 		c.gridwidth = 2; // 2 columns wide
 		c.gridy = 2; // third row
 		pane.add(button, c);
